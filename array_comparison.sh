1bcfc3b6fc7e9ecef5075370d24808fef9697e00
wilfriedmuffins <wilfried.moffen@live.fr>
2020-02-28 09:16:05 +0100

    exos done

--- array_comparison.sh
+++ array_comparison.sh
@@ -2,22 +2,22 @@ a=(3 5 8 10 6)
 b=(6 5 4 12)
 c=(14 7 5 7)
 common=0
-
-for N in ${a[@]} ; do 
- echo $common
-	for X in ${b[@]} ; do
-    	
-    	if [ $N == $X ] ; then
-        
-        remplir un autre tableau
-        
-        	for Y in ${c[@]} ; do
-            
-            	if [ $X == $Y ] ; then
-                		common=$C
-                fi
-            done
-        fi
-   done
+index=0
+for N in ${a[@]} ; do
+	for X in ${b[@]} ; do	
+        	#remplir un autre tableau
+		if [ $N == $X ] ; then
+			z[index]=$X
+			echo ${z[@]}
+			index=$(($index + 1))
+    	   	fi
+	done
+done
+for Y in ${c[@]} ; do
+	for Z in ${z[@]} ; do
+		if [ $Y == $Z ] ; then
+			common=$Y
+		fi
+	done
 done
 echo $common
